9e4964fd2c9fec017054020c2fd3c14bd678fc4f
Jared A Sutton <jaredasutton@gmail.com>
2023-07-26 23:18:08 +0000

    fix: Update 03 suite.

--- test/03_about_querying.js
+++ test/03_about_querying.js
@@ -1,47 +1,48 @@
 import test from 'node:test';
 import assert from 'node:assert';
-import {Observable} from 'rxjs';
+import {range, fromEvent, toArray, filter, map, bufferCount} from 'rxjs';
 import {EventEmitter} from 'events';
 
 // QUnit.module('Querying');
 
-var __ = 'Fill in the blank';
+const __ = 'Fill in the blank';
 
 test('Basic querying', function () {
-  var strings = [];
-  var numbers = Observable.range(1, 100);
+  const strings = [];
+  const numbers = range(1, 100);
 
-  numbers
-    .filter(function (x) { return x % __ === 0 })
-    .map(function (x) { return x.toString() })
-    .toArray()
-    .subscribe(strings.push.bind(strings));
+  numbers.pipe(
+    filter((x)=>(x % __ === 0)),
+    map((x)=>(x.toString())),
+    toArray()
+  )
+    .subscribe((x)=>{ strings.push(x); });
 
- assert.equal('11,22,33,44,55,66,77,88,99', strings.toString());
+  assert.equal('11,22,33,44,55,66,77,88,99', strings.toString());
 });
 
 test('querying over events', function () {
-  var results = 0;
+  let results = 0;
 
-  var e = new EventEmitter();
-  Observable.fromEvent(e, 'click')
-    .filter(function (click) { return click.x === click.y })
-    .map(function (click) { return __ + __; })
-    .subscribe(function (x) { results = x; });
+  const e = new EventEmitter();
+  fromEvent(e, 'click').pipe(
+    filter((click)=>(click.x === click.y)),
+    map((click)=>(__ + __))
+  ).subscribe(function (x) { results = x; });
 
   e.emit('click', {x: 100, y: 50});
   e.emit('click', {x: 75,  y: 75});
   e.emit('click', {x: 40,  y: 80});
 
- assert.equal(results, 150);
+  assert.equal(results, 150);
 });
 
-test('buffering with count and skip', function () {
-  var results = [];
-  Observable.range(1, 10)
-    .bufferWithCount(__, __)
-    .subscribe(results.push.bind(results));
+test.only('buffering with count and skip', function () {
+  const results = [];
+  range(1, 10).pipe(
+    bufferCount(__, __)
+  ).subscribe((x)=>{ results.push(x); });
 
- assert.equal('12345',  results[0].join(''));
- assert.equal('678910', results[1].join(''));
+  assert.equal('12345',  results[0].join(''));
+  assert.equal('678910', results[1].join(''));
 });
