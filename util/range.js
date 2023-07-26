054b4a89e6bafc8e3b50c3d7b84d96069b12efac
Jared A Sutton <jaredasutton@gmail.com>
2023-07-26 18:58:52 +0000

    fix: Fix 'Range' instantation for descending range.

--- util/range.js
+++ util/range.js
@@ -1,7 +1,7 @@
 export const Range = {
   create: function (start, end) {
     const direction = start < end ? 1 : -1;
-    return new Array(end - start + 1)
+    return new Array(Math.abs(end - start) + 1)
       .fill(null)
       .map(
         (_,i)=>(start + (direction*i))
